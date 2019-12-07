a870bdc0fc983c88eceb09a5c699ebc8931a63a1
Aramis Razzaghipour <aramisnoah@gmail.com>
2019-12-07 22:01:01 +1100

    Day 1 Part 2

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -2,8 +2,26 @@ use std::str::FromStr;
 
 const INPUT: &str = include_str!("day_1_input");
 
+fn calc_shallow_fuel(mass: u32) -> u32 {
+    let fuel = i64::from(mass) / 3 - 2;
+
+    if fuel > 0 {
+        fuel as u32
+    } else {
+        0
+    }
+}
+
 fn calc_fuel(mass: u32) -> u32 {
-    mass / 3 - 2
+    let mut iter_fuel = mass;
+    let mut total_fuel = 0;
+
+    while iter_fuel > 0 {
+        iter_fuel = calc_shallow_fuel(iter_fuel);
+        total_fuel += iter_fuel;
+    }
+
+    total_fuel
 }
 
 fn main() {
